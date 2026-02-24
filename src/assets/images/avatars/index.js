6b9c5d347fea87ff282d264859b9093206c4667f
Serivia <yenhoangle0805@gmail.com>
2026-02-24 11:20:43 -0500

    Clean up the list of people in the member's lists

--- src/assets/images/avatars/index.js
+++ src/assets/images/avatars/index.js
@@ -20,26 +20,7 @@ import maxHutton from './max-hutton.jpg';
 import mayaHutzul from './maya-hutzul.jpg';
 import miraSeth from './mira-seth.jpg';
 import muhammedQadeer from './muhammad-qadeer.jpg';
-import nadineTelsford from './nadine-telesford.jpg';
-import natalieLang from './natalie-lang.jpg';
-import nathanPerlman from './nathan-perlman.jpg';
 import noahPark from './noah-park.jpg';
-import rababbPannu from './rababb-pannu.jpg';
-import ryanGordon from './ryan-gordon.jpg';
-import ryanLiu from './ryan-liu.jpg';
-import sanaaMahmud from './sanaa-mahmud.jpg';
-import siaMehta from './sia-mehta.jpg';
-import siyaSani from './siya-sahni.jpg';
-import sjain from './sjain.jpg';
-import skylerXiang from './skyler-xiang.jpg';
-import ssagar from './ssagar.jpg';
-import surbhiRao from './surbhi-rao.jpg';
-import susanLi from './susan-li.jpg';
-import susannaLiao from './susanna-liao.jpg';
-import tessaWilliams from './tessa-williams.jpg';
-import theoBoychev from './theo-boychev.jpg';
-import emmaBedja from './emma-bedja.jpg';
-import agasthyaChidambaram from './agasthya-chidambaram.png';
 import dhruvPatel from './dhruv-patel.jpg';
 import cierraAlagnaf from './cierra-alagnaf.jpg';
 import danielMok from './daniel-mok.jpg';
@@ -56,12 +37,7 @@ import haristaaSatheeskumar from './haristaa-satheeskumar.jpg';
 import nusratRaisa from './nusrat-raisa.jpg';
 
 export {
-  aidanSchreder,
-  alishaWong,
-  anneliAloeCuffe,
-  christineLee,
   defaultImage,
-  dianaBrebeanu,
   ellieJiang,
   emilyPark,
   fardowsaMohamed,
@@ -79,38 +55,13 @@ export {
   mayaHutzul,
   miraSeth,
   muhammedQadeer,
-  nadineTelsford,
-  natalieLang,
-  nathanPerlman,
   noahPark,
-  rababbPannu,
-  ryanGordon,
-  ryanLiu,
-  sanaaMahmud,
-  siaMehta,
-  siyaSani,
-  sjain,
-  skylerXiang,
-  ssagar,
-  surbhiRao,
-  susanLi,
-  susannaLiao,
-  tessaWilliams,
-  theoBoychev,
-  emmaBedja,
-  agasthyaChidambaram,
   dhruvPatel,
-  elliotPagnan,
   zoeSavicJovcic,
-  georgiaWu,
   danielMok,
-  cierraAlagnaf,
   pennyMcClement,
   ethanLin,
-  noahDominguez,
   isaiahChambers,
-  haristaaSatheeskumar,
   fatimaQadeer,
   jasmineBasra,
-  nusratRaisa,
 };
